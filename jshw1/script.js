4d8a2cc4f9de974676a1e57f19070f7feb3d50c1
romartynets <33761259+romartynets@users.noreply.github.com>
2017-12-07 02:23:34 +0200

    Update script.js

--- jshw1/script.js
+++ jshw1/script.js
@@ -1,17 +1,43 @@
-var name,time,place,event,euro,dollar,usd,eur,a,b,c;
-
+var name,time,place,event,euro,dollar,usd,eur,a,b,c,znach;
+znach=/^[а-яА-Яa-zA-Z]+$/;
 name= prompt("Name:");
+while(1){
+  if(znach.test(name))
+  {
+    break
+  }
+  alert("Enter name");
+  name= prompt("Name:");  
+       }
+
 time= prompt("Time:");
+while(1){
+  if (/\d/.test(time) && time>0 && time<=24)
+  {
+    break
+  }
+  alert("Enter time(0-24):");
+  time= prompt("Time:");
+      }
+
 place= prompt("Place:");
 event= prompt("Event:");
-console.log(name+" has a "+event+" "+time+" "+place+".");
+console.log(name+" has a "+event+" at "+time+" in "+place+".");
 euro= prompt("Euros:");
-while (euro < '0' || euro > '9') {
+while (1) {
+  if (euro > '0' || !euro < '9')
+  {break}
     alert("Enter numbers");
     euro= prompt("Euros:");
   }
+  
 dollar= prompt("Dollars:");
-while (dollar < '0' || dollar > '9') {
+while (1) 
+  {
+    if (dollar > '0' || dollar < '9')
+    {
+      break
+    }
     alert("Enter numbers");
     dollar= prompt("Dollars:");
   }
@@ -21,7 +47,6 @@ a=eur*euro;
 b=dollar*usd;
 c=eur/usd;
 alert(euro+" euros are equal "+a+" grns, "+dollar+" dollars are equal "+b+" grns, one euro is equal "+c+" dollars.");
-
 
 
 
